3f521a6e2fa3cf53ece59b2230f9b80b4cb9f463
869117622@qq.com <your@email.com>
2016-08-28 18:48:07 +0800

    初始化项目

---
+++ 生活圈/生活圈/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  生活圈
+//
+//  Created by 1 10 1 on 16/8/28.
+//  Copyright © 2016年 1 10 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 生活圈/生活圈/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  生活圈
+//
+//  Created by 1 10 1 on 16/8/28.
+//  Copyright © 2016年 1 10 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
